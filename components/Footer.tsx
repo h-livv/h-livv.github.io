966229cbf6de730331b9f5970edfe17a0a8dffd2
Harliv Singh <harliv.research@gmail.com>
2026-08-02 00:49:40 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-8 px-6 md:px-12 border-t border-white/[0.05]">
       <div className="max-w-6xl mx-auto flex items-center justify-start text-xs text-secondary">
-        <p>© {new Date().getFullYear()} HARLIV. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} HARLIV SINGH.</p>
       </div>
     </footer>
   );
